bf5b9fb520721d02f3da9c990fe9481e809f2a48
anbinx <klosyx@outlook.com>
2023-12-06 11:09:01 +0800

    Add: checkbox group to select specific tools.

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,8 +1,15 @@
 from __future__ import annotations
+
 __all__ = [
     'FakeNewsDetectionTool',
     'ImageComprehendingTool',
+    'TOOL_LIST',
 ]
 
 from .fake_news_detection_tool import FakeNewsDetectionTool
 from .multi_modal_content_comprehending import ImageComprehendingTool
+
+TOOL_LIST = [
+    FakeNewsDetectionTool,
+    ImageComprehendingTool,
+]
