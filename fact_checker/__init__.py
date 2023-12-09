5a5e8395bd5e5a827eff6ece978fa06386fba304
AnbinX <46080273+KLOSYX@users.noreply.github.com>
2023-12-09 17:35:36 +0800

    Fix: fact-checker agent prompt.

--- fact_checker/__init__.py
+++ fact_checker/__init__.py
@@ -74,8 +74,9 @@ def get_fact_checker_chain():
     return chain
 
 
-agent_template = """You are a professional fact checker. Given the following tweet text, \
-please judge whether the tweet is true or false and give your reasons step by step.
+agent_template = """You are a professional fact checker. Given the following tweet text \
+and tweet image path, please judge whether the tweet is true or false and \
+give your reasons step by step.
 tweet text: {tweet_text}
 tweet image path: {tweet_image_path}"""
 
