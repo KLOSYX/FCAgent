012d88956a39f3f095892da4352461adfbafd3e5
anbinx <klosyx@outlook.com>
2024-03-19 15:27:36 +0800

    add more agent types

--- config/__init__.py
+++ config/__init__.py
@@ -11,10 +11,14 @@ from typing import Literal
 
 @dataclass(frozen=True)
 class Config:
+    log_level: Literal["DEBUG", "INFO", "WARNING"] = "DEBUG"
+    agent_type: Literal[
+        "openai_tools", "react_json", "structured_chat", "shoggoth13_react_json"
+    ] = "shoggoth13_react_json"
     core_server_addr: str = "http://10.26.128.30:8000"
     vl_model_type: Literal["local", "gpt4v"] = "gpt4v"
     vl_server_addr: str = "http://10.26.128.30:8001"
-    model_name: str = "gpt-3.5-turbo"
+    model_name: str = "gpt-4-0125-preview"
     search_engine: str = "bing"
     web_scrapy_max_splits: int = 3
 
--- fact_checker/__init__.py
+++ fact_checker/__init__.py
@@ -5,30 +5,35 @@ import operator
 from datetime import datetime
 from typing import Annotated, List, TypedDict, Union
 
-from langchain.agents import AgentExecutor, create_openai_tools_agent
+from agents_deconstructed import react_chat
+from agents_deconstructed.format_tools import format_tools_args
+from langchain.agents import (
+    create_json_chat_agent,
+    create_openai_tools_agent,
+    create_structured_chat_agent,
+)
 from langchain.prompts import MessagesPlaceholder, PromptTemplate
 from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.messages import BaseMessage
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI
 from langgraph.graph import END, StateGraph
 from langgraph.prebuilt.tool_executor import ToolExecutor
+from loguru import logger
 from pyrootutils import setup_root
 
 from config import Config
 
 __all__ = ["get_fact_checker_agent"]
 config = Config()
+logger.level(config.log_level)
 
 ROOT = setup_root(".")
 
-agent_template = """你是一名专业的事实核查机构编辑，给定如下的推文文本内容 \
-以及推文图片路径，请首先将内容分解为待核查的子问题清单:
-1. xxx
-2. xxx
-...
-随后借助工具，逐一核查子问题是否真实，并给出你的判断依据。 \
-所有子问题核查结束后，请用“核查结束：(你的结论)”结尾。
+agent_template = """你是一名专业的事实核查机构编辑，给定如下的推文文本内容\
+以及推文图片路径，请借助工具，核查问题是否真实，并给出你的判断依据。\
+核查结束后，请用“核查结束：(你的结论)”结尾。
 当前日期：{date}
 待核查文本：{tweet_text}
 待核查图片：{tweet_image_name}"""
@@ -45,8 +50,69 @@ agent_prompt = PromptTemplate(
 class AgentState(TypedDict):
     input: str
     chat_history: list[BaseMessage]
-    agent_outcome: AgentAction | AgentFinish | None
+    agent_outcome: list | AgentAction | AgentFinish | None
     intermediate_steps: Annotated[list[tuple[AgentAction, str]], operator.add]
+
+
+def _get_agent(agent_name: str, llm, tools):
+    if agent_name == "openai_tools":
+        with open(ROOT / "prompt" / "openai-tools-agent.json") as f:
+            prompt_raw = json.load(f)
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", prompt_raw["system"]),
+                MessagesPlaceholder("chat_history", optional=True),
+                ("human", prompt_raw["human"]),
+                MessagesPlaceholder("agent_scratchpad"),
+            ]
+        )
+        return create_openai_tools_agent(llm, tools, prompt)
+    elif agent_name == "react_json":
+        with open(ROOT / "prompt" / "react_json.json") as f:
+            prompt_raw = json.load(f)
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", prompt_raw["system"]),
+                MessagesPlaceholder("chat_history", optional=True),
+                ("human", prompt_raw["human"]),
+                MessagesPlaceholder("agent_scratchpad"),
+            ]
+        )
+        return create_json_chat_agent(llm, tools, prompt)
+    elif agent_name == "structured_chat":
+        with open(ROOT / "prompt" / "structured_chat.json") as f:
+            prompt_raw = json.load(f)
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", prompt_raw["system"]),
+                MessagesPlaceholder("chat_history", optional=True),
+                ("human", prompt_raw["human"]),
+            ]
+        )
+        return create_structured_chat_agent(llm, tools, prompt)
+    elif agent_name == "shoggoth13_react_json":
+        llm.bind(stop=["\nObservation"])
+        with open(ROOT / "prompt" / "shoggoth13_react_json.json") as f:
+            prompt_raw = json.load(f)
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", prompt_raw["system"]),
+                ("human", prompt_raw["human"]),
+                MessagesPlaceholder("intermediate_steps"),
+            ]
+        )
+        prompt = prompt.partial(
+            tools=format_tools_args(tools), tool_names=", ".join([tool.name for tool in tools])
+        )
+        agent = (
+            RunnablePassthrough.assign(
+                intermediate_steps=lambda x: react_chat.format_steps(x["intermediate_steps"]),
+            )
+            | prompt
+            | llm
+            | react_chat.ReActOutputParser()
+        )
+        return agent
 
 
 def get_fact_checker_agent(tools):
@@ -55,41 +121,45 @@ def get_fact_checker_agent(tools):
         model_name=config.model_name,
         streaming=True,
     )
-    with open(ROOT / "prompt" / "openai-tools-agent.json") as f:
-        prompt_raw = json.load(f)
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", prompt_raw["system"]),
-            MessagesPlaceholder("chat_history", optional=True),
-            ("human", prompt_raw["human"]),
-            MessagesPlaceholder("agent_scratchpad"),
-        ]
-    )
-    agent = create_openai_tools_agent(llm, tools, prompt)
+    agent = _get_agent(config.agent_type, llm, tools)
 
     async def init_agent(data):
         inputs = data["input"]
+        logger.debug(f"init agent with agent state: {data}")
         msg = await agent_prompt.ainvoke(inputs)
         return {"input": msg.text, "intermediate_steps": [], "chat_history": []}
 
     async def run_agent(data):
+        logger.debug(f"run agent with agent state: {data}")
         agent_outcome = await agent.ainvoke(data)
         return {"agent_outcome": agent_outcome}
 
     async def execute_tools(data):
+        logger.debug(f"execute tools with agent state: {data}")
         agent_actions = data["agent_outcome"]
         steps = []
-        for action in agent_actions:
-            output = await tool_executor.ainvoke(action)
-            steps.append((action, str(output)))
-        agent_actions.clear()
+        if isinstance(agent_actions, list):
+            for action in agent_actions:
+                output = await tool_executor.ainvoke(action)
+                steps.append((action, str(output)))
+            agent_actions.clear()
+        else:
+            output = await tool_executor.ainvoke(agent_actions)
+            steps.append((agent_actions, str(output)))
+            data["agent_outcome"] = None
         return {"intermediate_steps": steps}
 
     def should_continue(data):
+        logger.debug(f"should continue with agent state: {data}")
         if isinstance(data["agent_outcome"], AgentFinish):
-            if "核查结束" in data["agent_outcome"].messages[0].content:
+            if (
+                "核查结束" in data["agent_outcome"].messages[0].content
+                or config.agent_type == "shoggoth13_react_json"
+            ):
+                logger.debug("agent stopped")
                 return "end"
             else:
+                logger.debug("agent re-enter")
                 return "agent"
         else:
             return "continue"
